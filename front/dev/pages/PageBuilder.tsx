e7c41c2724a81929f32d42da7c584357693994f0
yuki-k112 <60835872+yuki-k112@users.noreply.github.com>
2021-08-18 00:03:17 +0900

    ルーティング関連の追加

---
+++ front/dev/pages/PageBuilder.tsx
@@ -0,0 +1,4 @@
+export const ListPageBuilder = () => {
+    // todo: 各ページ間で共通している箇所（レイアウト等）をここで対応。
+    // containerコンポーネントを返すコンポーネントとして実装
+}
---
+++ front/dev/pages/todo/todo.page.tsx
@@ -0,0 +1,7 @@
+import { TodoListContainer } from "~/apps/todo/component/TodoList";
+
+export const TodoListPage = () => (
+    // todo:PageBuliderコンポーネントを使用
+    // <PageBuilder body={TodoListContainer} /> のように実装
+    <TodoListContainer />
+)
