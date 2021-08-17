6b9e5e8ab1dcb05af2d298668a1b34fffb847777
yuki-k112 <60835872+yuki-k112@users.noreply.github.com>
2021-08-18 00:02:57 +0900

    todo コンポーネント実装

---
+++ front/dev/apps/todo/component/TodoList.tsx
@@ -0,0 +1,71 @@
+import React, { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '~/apps/store';
+import { FetchWrapper } from '~/commonComponent/wrapper/FetchWrapper';
+import { asyncFetchTodoList } from '../presenter/todo.action';
+import { todoListSelector } from '../presenter/todo.selector';
+import { todoSlice } from '../presenter/todo.slice';
+
+
+/**
+ * Type
+ */
+
+type TodoListProps = {
+    list: {
+        title: string;
+        id: string;
+        count:string;
+    }[];
+    handleClick: (id: TodoListProps['list'][number]['id']) => () => void;
+};
+
+/**
+ * style
+ */
+
+// todo:emotionによるスタイル実装
+
+
+/**
+ * presentational component
+ */
+
+export const TodoList: FC<TodoListProps> = ({ list, handleClick }) => (
+    <>
+        <ul>
+            {list.map((item) => (
+                <li key={item.id}>
+                    <p>{item.count}</p>
+                    <p>{item.title}</p>
+                    <button type="button" onClick={handleClick(item.id)}>
+                        delete
+                    </button>
+                </li>
+            ))}
+        </ul>
+    </>
+);
+
+/**
+ * container component
+ */
+
+export const TodoListContainer = () => {
+    const dispatch = useDispatch();
+    const handleClick = (id: TodoListProps['list'][number]['id']) => () => dispatch(todoSlice.actions.deleteToDo(id));
+    const list = useSelector(todoListSelector)
+    const isBeforeFetch = useSelector((store:RootState) => store.todo.isBeforeFetch)
+    const isFetchFailed = useSelector((store:RootState) => store.todo.isFetchFailed)
+
+    useEffect(() => {
+        dispatch(asyncFetchTodoList())
+    },[dispatch])
+
+    return (
+        <FetchWrapper isFailed={isFetchFailed} isLoading={isBeforeFetch} >
+            <TodoList handleClick={handleClick} list={list} />
+        </FetchWrapper>
+    )
+
+}
