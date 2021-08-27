08798d9cad31222925943d99cbfbfb482b8d4391
yuki-k112 <60835872+yuki-k112@users.noreply.github.com>
2021-08-28 03:22:58 +0900

    prettier対応

--- front/dev/apps/todo/presenter/todo.selector.ts
+++ front/dev/apps/todo/presenter/todo.selector.ts
@@ -1,7 +1,7 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "~/apps/store";
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '~/apps/store';
 
 export const todoListSelector = createSelector(
-    (state:RootState) => state.todo.list,
-    (list) => list.map((item,i) => ({...item, count:`No.${i}`}))
-)
+    (state: RootState) => state.todo.list,
+    (list) => list.map((item, i) => ({ ...item, count: `No.${i}` })),
+);
