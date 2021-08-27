058b5fd2c27d0c63f19d93967819df21e5717c7c
yuki-k112 <60835872+yuki-k112@users.noreply.github.com>
2021-08-28 03:23:50 +0900

    todoRepository リファクタリング対応

--- front/dev/apps/todo/core/todo.repository.ts
+++ front/dev/apps/todo/core/todo.repository.ts
@@ -1,13 +1,7 @@
-import { apiClient } from '~/infrastructure/apiClient';
+import httpClient from '~/infrastructure/apiClient';
 import { buildGetRepository } from '~/infrastructure/buildRepository';
-import { TodoListApiInput, TodoListApiOutput } from './todo.apiType';
+import { TodoListApiOutput } from './todo.apiType';
 
 const toFrontData = (data: TodoListApiOutput) => data.map((item) => ({ id: item.id.toString(), title: item.title }));
 
-const buildFetchTodoList = <T, T2>(converter: (response: T) => T2) => async (config?: TodoListApiInput) => {
-    const repository = buildGetRepository(apiClient('get'))('todos', config);
-
-    return converter(await repository());
-};
-
-export const todoRepository = { fetchTodoList: buildFetchTodoList(toFrontData) };
+export const todoRepository = { fetchList: buildGetRepository(httpClient)(toFrontData)('todos') };
